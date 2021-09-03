6b963f51e5c2a01b052d956ad8717a1cd28fd3c6
Nick Girardo <nickgirardo@gmail.com>
2021-09-03 07:42:38 -0400

    TeX Loader: Properly handling packages

--- src/loaderTeX.js
+++ src/loaderTeX.js
@@ -5,12 +5,24 @@ import { SVG } from 'mathjax-full/js/output/svg';
 import { liteAdaptor } from 'mathjax-full/js/adaptors/liteAdaptor';
 import { RegisterHTMLHandler } from 'mathjax-full/js/handlers/html';
 
-const adaptor = liteAdaptor();
-RegisterHTMLHandler(adaptor);
+import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages';
 
-export function loadTeX(source, options) {
+const defaultOptions = {
+  lang: 'TeX',
+  packages: ['base', 'ams'],
+  allPackages: false,
+};
+
+export function loadTeX(source, userOptions) {
+  const options = { ...defaultOptions, ...userOptions };
+
+  // If we are told to include all packages, ignore the packages arg and use all
+  const packages = options.allPackages ? AllPackages : options.packages;
+
+  const adaptor = liteAdaptor();
+  RegisterHTMLHandler(adaptor);
   // TODO take packages from options
-  const tex = new TeX({ packages: ['base', 'ams'] });
+  const tex = new TeX({ packages });
   const svg = new SVG({ fontCache: 'none' });
 
   const texDoc = mathjax.document('', {
